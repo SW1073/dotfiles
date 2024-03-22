9865ff30d3015ca8c191667b5f1a5afd2175c5b3
SW1073 <eloiimerino@gmail.com>
2024-03-22 13:06:07 +0100

    Executar modelsim dins de la mateixa imatge que quartus :)

---
+++ misc_scripts/bin/exe_modelsim.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+sudo docker run\
+    --rm\
+    -ti\
+    -v /tmp/.X11-unix:/tmp/.X11-unix:rw\
+    -v $(pwd):/home/usuari:rw\
+    -e DISPLAY=$DISPLAY\
+    -e LD_LIBRARY_PATH=/root/altera/13.0sp1/modelsim_ase/lib32\
+    -e HOME=/home/usuari\
+    --user=$(id -u):$(id -g)\
+    --ipc=host --cap-add=SYS_PTRACE\
+    --security-opt seccomp=unconfined\
+    registry.gitlab.com/axtaor/practicas-ac2:latest\
+    /root/altera/13.0sp1/modelsim_ase/linux/vsim
