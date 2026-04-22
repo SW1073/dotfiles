238090d71a9b3aead79ae51e1ca1af4da00a0f57
SW1073 <eloiimerino@gmail.com>
2026-04-23 00:18:58 +0200

    feat(nvim): more LSP and lsp-format QOL plugingit status1!

--- nvim/.config/nvim/lua/default/plugin/lsp.lua
+++ nvim/.config/nvim/lua/default/plugin/lsp.lua
@@ -1,27 +1,40 @@
 vim.pack.add({
-  { src = "https://github.com/neovim/nvim-lspconfig" },
-  { src = "https://github.com/hrsh7th/cmp-nvim-lsp" },
-  { src = "https://github.com/hrsh7th/nvim-cmp" },
+    { src = "https://github.com/neovim/nvim-lspconfig" },
+    { src = "https://github.com/hrsh7th/cmp-nvim-lsp" },
+    { src = "https://github.com/hrsh7th/nvim-cmp" },
+    { src = "https://github.com/lukas-reineke/lsp-format.nvim" },
 })
 
+-- Configure lsp-format
+require("lsp-format").setup({})
+-- Register autocomand to let it interact with the available lsp server
+vim.api.nvim_create_autocmd('LspAttach', {
+    callback = function(args)
+        local client = assert(vim.lsp.get_client_by_id(args.data.client_id))
+        require("lsp-format").on_attach(client, args.buf)
+    end,
+})
+
+-- Enable clangd
 vim.lsp.config("clangd", {
-  cmd = { "clangd" },
-  filetypes = { "c", "cpp", "objc", "objcpp" },
-  root_markers = { ".clangd", "compile_commands.json", "compile_commands.yaml", "Makefile", ".git" },
+    cmd = { "clangd" },
+    filetypes = { "c", "cpp", "objc", "objcpp" },
+    root_markers = { ".clangd", "compile_commands.json", "compile_commands.yaml", "Makefile", ".git" },
 })
 vim.lsp.enable("clangd")
 
-
--- vim.lsp.config("pyright", {
---     cmd = {"pyright-langserver"},
---     filetypes = { "py" },
---     root_markers = { "Makefile", ".git" },
--- })
+-- Enable pyright
 vim.lsp.enable("pyright")
 
-
+-- Enable bashls
 vim.lsp.config.bashls = {
-  cmd = { 'bash-language-server', 'start' },
-  filetypes = { 'bash', 'sh' }
+    cmd = { 'bash-language-server', 'start' },
+    filetypes = { 'bash', 'sh' }
 }
-vim.lsp.enable 'bashls'
+vim.lsp.enable("bashls")
+
+-- Enable luals
+vim.lsp.enable("lua_ls")
+
+-- Enable svls
+vim.lsp.enable("svls")
