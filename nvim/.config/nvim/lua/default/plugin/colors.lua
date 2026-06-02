50d0211fa7e208adc79dbdf930dfc7520af2ddf9
SW1073 <eloiimerino@gmail.com>
2026-06-02 09:44:41 +0200

    fix(nvim): built-in treesitter and colors.lua linting

--- nvim/.config/nvim/lua/default/plugin/colors.lua
+++ nvim/.config/nvim/lua/default/plugin/colors.lua
@@ -3,10 +3,10 @@ vim.pack.add({
     { src = 'https://github.com/projekt0n/github-nvim-theme' },
     { src = 'https://github.com/shaunsingh/solarized.nvim' },
     { src = 'https://github.com/tanvirtin/monokai.nvim' },
-	{ src = 'https://github.com/folke/tokyonight.nvim' },
-	{ src = 'https://github.com/blazkowolf/gruber-darker.nvim' },
+    { src = 'https://github.com/folke/tokyonight.nvim' },
+    { src = 'https://github.com/blazkowolf/gruber-darker.nvim' },
     { src = 'https://github.com/rebelot/kanagawa.nvim' },
-    { src = 'https://github.com/LuRsT/austere.vim'},
+    { src = 'https://github.com/LuRsT/austere.vim' },
     { src = 'https://github.com/lunacookies/vim-colors-xcode' },
     { src = 'https://github.com/olimorris/onedarkpro.nvim' },
     { src = 'https://github.com/Mofiqul/vscode.nvim' },
@@ -25,13 +25,14 @@ function ConfigureGruvbox()
             emphasis = true,
             comments = true,
             operators = false,
-            folds = true, },
+            folds = true,
+        },
         strikethrough = true,
         invert_selection = false,
         invert_signs = false,
         invert_tabline = false,
         invert_intend_guides = false,
-        inverse = true, -- invert background for search, diffs, statuslines and errors
+        inverse = true,    -- invert background for search, diffs, statuslines and errors
         contrast = "hard", -- can be "hard", "soft" or empty string
         palette_overrides = {},
         overrides = {},
@@ -47,14 +48,14 @@ function ConfigureGitHubCS()
             -- Compiled file's destination location
             compile_path = vim.fn.stdpath('cache') .. '/github-theme',
             compile_file_suffix = '_compiled', -- Compiled file suffix
-            hide_end_of_buffer = true, -- Hide the '~' character at the end of the buffer for a cleaner look
-            hide_nc_statusline = true, -- Override the underline style for non-active statuslines
-            transparent = false,       -- Disable setting background
-            terminal_colors = true,    -- Set terminal colors (vim.g.terminal_color_*) used in `:terminal`
-            dim_inactive = false,      -- Non focused panes set to alternative background
-            module_default = true,     -- Default enable value for modules
-            styles = {                 -- Style to be applied to different syntax groups
-                comments = 'NONE',       -- Value is any valid attr-list value `:help attr-list`
+            hide_end_of_buffer = true,         -- Hide the '~' character at the end of the buffer for a cleaner look
+            hide_nc_statusline = true,         -- Override the underline style for non-active statuslines
+            transparent = false,               -- Disable setting background
+            terminal_colors = true,            -- Set terminal colors (vim.g.terminal_color_*) used in `:terminal`
+            dim_inactive = false,              -- Non focused panes set to alternative background
+            module_default = true,             -- Default enable value for modules
+            styles = {                         -- Style to be applied to different syntax groups
+                comments = 'NONE',             -- Value is any valid attr-list value `:help attr-list`
                 functions = 'NONE',
                 keywords = 'NONE',
                 variables = 'NONE',
@@ -65,19 +66,19 @@ function ConfigureGitHubCS()
                 strings = 'NONE',
                 types = 'NONE',
             },
-            inverse = {                -- Inverse highlight for different types
+            inverse = { -- Inverse highlight for different types
                 match_paren = false,
                 visual = false,
                 search = false,
             },
-            darken = {                 -- Darken floating windows and sidebar-like windows
+            darken = { -- Darken floating windows and sidebar-like windows
                 floats = false,
                 sidebars = {
                     enable = true,
-                    list = {},             -- Apply dark background to specific windows
+                    list = {}, -- Apply dark background to specific windows
                 },
             },
-            modules = {                -- List of various plugins and additional options
+            modules = { -- List of various plugins and additional options
                 -- ...
             },
         },
@@ -91,9 +92,9 @@ function ConfigureTokyoNight()
     require("tokyonight").setup({
         -- your configuration comes here
         -- or leave it empty to use the default settings
-        style = "storm", -- The theme comes in three styles, `storm`, `moon`, a darker variant `night` and `day`
-        light_style = "day", -- The theme is used when the background is set to light
-        transparent = true, -- Enable this to disable setting the background color
+        style = "storm",        -- The theme comes in three styles, `storm`, `moon`, a darker variant `night` and `day`
+        light_style = "day",    -- The theme is used when the background is set to light
+        transparent = true,     -- Enable this to disable setting the background color
         terminal_colors = true, -- Configure the colors used when opening a `:terminal` in [Neovim](https://github.com/neovim/neovim)
         styles = {
             -- Style to be applied to different syntax groups
@@ -103,14 +104,14 @@ function ConfigureTokyoNight()
             functions = {},
             variables = {},
             -- Background styles. Can be "dark", "transparent" or "normal"
-            sidebars = "dark", -- style for sidebars, see below
-            floats = "dark", -- style for floating windows
+            sidebars = "dark",            -- style for sidebars, see below
+            floats = "dark",              -- style for floating windows
         },
-        sidebars = { "qf", "help" }, -- Set a darker background on sidebar-like windows. For example: `["qf", "vista_kind", "terminal", "packer"]`
-        day_brightness = 0.3, -- Adjusts the brightness of the colors of the **Day** style. Number between 0 and 1, from dull to vibrant colors
+        sidebars = { "qf", "help" },      -- Set a darker background on sidebar-like windows. For example: `["qf", "vista_kind", "terminal", "packer"]`
+        day_brightness = 0.3,             -- Adjusts the brightness of the colors of the **Day** style. Number between 0 and 1, from dull to vibrant colors
         hide_inactive_statusline = false, -- Enabling this option, will hide inactive statuslines and replace them with a thin border instead. Should work with the standard **StatusLine** and **LuaLine**.
-        dim_inactive = false, -- dims inactive windows
-        lualine_bold = false, -- When `true`, section headers in the lualine theme will be bold
+        dim_inactive = false,             -- dims inactive windows
+        lualine_bold = false,             -- When `true`, section headers in the lualine theme will be bold
 
         --- You can override specific color groups to use other groups or a hex color
         --- function will be called with a ColorScheme table
@@ -127,26 +128,26 @@ end
 
 function ConfigureKanagawa()
     require('kanagawa').setup({
-        compile = false,             -- enable compiling the colorscheme
-        undercurl = true,            -- enable undercurls
+        compile = false,  -- enable compiling the colorscheme
+        undercurl = true, -- enable undercurls
         commentStyle = { italic = true },
         functionStyle = {},
-        keywordStyle = { italic = true},
+        keywordStyle = { italic = true },
         statementStyle = { bold = true },
         typeStyle = {},
-        transparent = false,         -- do not set background color
-        dimInactive = false,         -- dim inactive window `:h hl-NormalNC`
-        terminalColors = true,       -- define vim.g.terminal_color_{0,17}
-        colors = {                   -- add/modify theme and palette colors
+        transparent = false,   -- do not set background color
+        dimInactive = false,   -- dim inactive window `:h hl-NormalNC`
+        terminalColors = true, -- define vim.g.terminal_color_{0,17}
+        colors = {             -- add/modify theme and palette colors
             palette = {},
             theme = { wave = {}, lotus = {}, dragon = {}, all = {} },
         },
         overrides = function(colors) -- add/modify highlights
             return {}
         end,
-        theme = "wave",              -- Load "wave" theme when 'background' option is not set
-        background = {               -- map the value of 'background' option to a theme
-            dark = "wave",           -- try "dragon" !
+        theme = "wave",    -- Load "wave" theme when 'background' option is not set
+        background = {     -- map the value of 'background' option to a theme
+            dark = "wave", -- try "dragon" !
             light = "lotus"
         },
     })
@@ -176,6 +177,5 @@ function ColorMyPencils(color)
     -- vim.api.nvim_set_hl(0, "Normal", {bg = "none"})
     -- vim.api.nvim_set_hl(0, "NormalFloat", {bg = "none"})
 end
-
 
 ColorMyPencils()
